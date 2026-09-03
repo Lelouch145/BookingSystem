0eaf10a30a78c307f01ff63b4de4b58e457bef72
Lelouch145 <150359357+Lelouch145@users.noreply.github.com>
2026-09-03 14:36:00 +0200

    Refactor JWT service configuration and token creation

--- BookingSystem.Api/Services/JWTService.cs
+++ BookingSystem.Api/Services/JWTService.cs
@@ -4,6 +4,7 @@ using System.Text;
 using BookingSystem.Api.Models.SystemModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualBasic;
 
 namespace BookingSystem.Api.Services;
 
@@ -19,6 +20,30 @@ public class JWTService
     }
     public async Task<string> TokenCreationAsync(ApplicationUser user)
     {
+        var claims = await CreateClaimsAsync(user);
+
+        var credentials = CreateSigningsCredentials();
+        var issuer = _config["JWT:Issuer"] ?? throw new InvalidOperationException("JWT issuer info is missing");
+        var audience = _config["JWT:Audience"] ?? throw new InvalidOperationException("JWT Audience info is missing");
+        var expirationMinutes = _config.GetValue<int?>("JWT:ExpirationMinutes") ?? throw new InvalidOperationException("JWT expiration time is missing");
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+
+            claims: claims,
+
+            
+            expires: DateTime.UtcNow.AddMinutes(expirationMinutes),
+
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
+    }
+
+    private async Task<List<Claim>> CreateClaimsAsync(ApplicationUser user)
+    {
         var roles = await _userManager.GetRolesAsync(user);
 
         var claims = new List<Claim>
@@ -32,26 +57,18 @@ public class JWTService
             claims.Add(new Claim(ClaimTypes.Role, role));
         }
 
+        return claims;
+    }
+    
+    private SigningCredentials CreateSigningsCredentials()
+    {
         var jwtKey = _config["JWT:KEY"] ?? throw new InvalidOperationException("JWT key is missing");
 
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
 
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        var issuer = _config["JWT:Issuer"] ?? throw new InvalidOperationException("JWT issuer info is missing");
-        var audience = _config["JWT:Audience"] ?? throw new InvalidOperationException("JWT Audience info is missing");
-        var token = new JwtSecurityToken(
-            issuer: issuer,
-            audience: audience,
 
-            claims: claims,
-
-            expires: DateTime.UtcNow.AddMinutes(60),
-
-            signingCredentials: credentials
-        );
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-        
+        return credentials;
     }
 }
